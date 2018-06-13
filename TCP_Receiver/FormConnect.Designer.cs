97abb030f37f0e860d89e1ba50ce161d71a660fe
Elliot Abbott <e.anslow.abbott@gmail.com>
2018-06-13 11:09:31 +0100

    Add project files.

---
+++ TCP_Receiver/FormConnect.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace TCP_Receiver
+{
+    partial class FormConnect
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxIp = new System.Windows.Forms.TextBox();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.labelIp = new System.Windows.Forms.Label();
+            this.labelPort = new System.Windows.Forms.Label();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.textBoxOut = new System.Windows.Forms.TextBox();
+            this.buttonDisconnect = new System.Windows.Forms.Button();
+            this.pictureBoxImage = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxIp
+            // 
+            this.textBoxIp.Location = new System.Drawing.Point(79, 20);
+            this.textBoxIp.Name = "textBoxIp";
+            this.textBoxIp.Size = new System.Drawing.Size(126, 20);
+            this.textBoxIp.TabIndex = 0;
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.Location = new System.Drawing.Point(265, 20);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(73, 20);
+            this.textBoxPort.TabIndex = 1;
+            // 
+            // labelIp
+            // 
+            this.labelIp.AutoSize = true;
+            this.labelIp.Location = new System.Drawing.Point(12, 23);
+            this.labelIp.Name = "labelIp";
+            this.labelIp.Size = new System.Drawing.Size(61, 13);
+            this.labelIp.TabIndex = 2;
+            this.labelIp.Text = "IP Address:";
+            // 
+            // labelPort
+            // 
+            this.labelPort.AutoSize = true;
+            this.labelPort.Location = new System.Drawing.Point(230, 23);
+            this.labelPort.Name = "labelPort";
+            this.labelPort.Size = new System.Drawing.Size(29, 13);
+            this.labelPort.TabIndex = 3;
+            this.labelPort.Text = "Port:";
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(366, 20);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(99, 20);
+            this.buttonConnect.TabIndex = 4;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.ButtonConnect_Click);
+            // 
+            // textBoxOut
+            // 
+            this.textBoxOut.Location = new System.Drawing.Point(15, 46);
+            this.textBoxOut.Multiline = true;
+            this.textBoxOut.Name = "textBoxOut";
+            this.textBoxOut.ReadOnly = true;
+            this.textBoxOut.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxOut.Size = new System.Drawing.Size(555, 108);
+            this.textBoxOut.TabIndex = 5;
+            // 
+            // buttonDisconnect
+            // 
+            this.buttonDisconnect.Location = new System.Drawing.Point(471, 20);
+            this.buttonDisconnect.Name = "buttonDisconnect";
+            this.buttonDisconnect.Size = new System.Drawing.Size(99, 20);
+            this.buttonDisconnect.TabIndex = 6;
+            this.buttonDisconnect.Text = "Disconnect";
+            this.buttonDisconnect.UseVisualStyleBackColor = true;
+            this.buttonDisconnect.Click += new System.EventHandler(this.ButtonDisconnect_Click);
+            // 
+            // pictureBoxImage
+            // 
+            this.pictureBoxImage.Location = new System.Drawing.Point(15, 160);
+            this.pictureBoxImage.Name = "pictureBoxImage";
+            this.pictureBoxImage.Size = new System.Drawing.Size(555, 312);
+            this.pictureBoxImage.TabIndex = 7;
+            this.pictureBoxImage.TabStop = false;
+            // 
+            // FormConnect
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(605, 484);
+            this.Controls.Add(this.pictureBoxImage);
+            this.Controls.Add(this.buttonDisconnect);
+            this.Controls.Add(this.textBoxOut);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.labelPort);
+            this.Controls.Add(this.labelIp);
+            this.Controls.Add(this.textBoxPort);
+            this.Controls.Add(this.textBoxIp);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormConnect";
+            this.Text = "TCP Connection";
+            this.Load += new System.EventHandler(this.FormConnect_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxIp;
+        private System.Windows.Forms.TextBox textBoxPort;
+        private System.Windows.Forms.Label labelIp;
+        private System.Windows.Forms.Label labelPort;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.TextBox textBoxOut;
+        private System.Windows.Forms.Button buttonDisconnect;
+        private System.Windows.Forms.PictureBox pictureBoxImage;
+    }
+}
+
---
+++ TCP_Receiver/formConnect.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.IO;
+
+namespace TCP_Receiver
+{
+    public partial class FormConnect : Form
+    {
+
+        Thread streamThread = null;
+        TcpClient client = null;
+
+        // Using JPEG file signature to identify where the JPG 
+        // file begins and ends in each packet.
+        byte[] jpegHead = new byte[] { 0xff, 0xd8 };
+        byte[] jpegTail = new byte[] { 0xff, 0xd9 };
+
+        public FormConnect()
+        {
+            InitializeComponent();
+        }
+
+        // automatically populates with test scenario
+        private void FormConnect_Load(object sender, EventArgs e)
+        {
+            textBoxIp.Text = "localhost";
+            textBoxPort.Text = "13000";
+            buttonDisconnect.Enabled = false;
+        }
+
+        // connect button starts the streaming thread
+        private void ButtonConnect_Click(object sender, EventArgs e)
+        {
+            buttonConnect.Enabled = false;
+            buttonDisconnect.Enabled = true;
+            textBoxIp.Enabled = false;
+            textBoxPort.Enabled = false;
+
+            streamThread = new Thread(new ThreadStart(ClientConnect));
+            streamThread.IsBackground = true;
+            streamThread.Start();
+        }
+
+        private void ClientConnect()
+        {
+            // databuffer handles all streamed data.
+            byte[] dataBuffer;
+            int bytesReceived;
+
+            try
+            {
+                string address = textBoxIp.Text;
+                int port = int.Parse(textBoxPort.Text);
+
+                client = new TcpClient(address, port);
+                NetworkStream stream = client.GetStream();
+                logText("Client connected");
+
+                do
+                {
+                    dataBuffer = new byte[client.ReceiveBufferSize];
+                    bytesReceived = stream.Read(dataBuffer, 0, client.ReceiveBufferSize);
+                    logText(bytesReceived.ToString() + " bytes recieved");
+
+                    processData(dataBuffer);
+
+                    //Thread.Sleep(10);
+                } while (true);
+            }
+            catch (SocketException e)
+            {
+                logText(e.Message);
+            }
+            catch (IOException e)
+            {
+                logText(e.Message);
+            }
+            catch (FormatException e)
+            {
+                logText(e.Message);
+            }
+            finally
+            {
+                if (client != null)
+                {
+                    client.Close();
+                }
+            }
+        }
+
+        // process each packet of data to produce the images
+        private void processData(byte[] data)
+        {
+            int imgLength;
+            int indexJpegHead = findIndex(data, jpegHead);
+            int indexJpegTail = findIndex(data, jpegTail);
+
+            if ((indexJpegHead != -1) && (indexJpegTail != -1) && (indexJpegHead < indexJpegTail))
+            {
+                //total bytes of image ( + 2 for JPEG tail )
+                imgLength = (indexJpegTail + 2) - indexJpegHead;
+                byte[] imgData = new byte[imgLength];
+                Array.Copy(data, indexJpegHead, imgData, 0, imgLength);
+
+                //autodisposes memStream
+                using (MemoryStream memStream = new MemoryStream(imgData))
+                {
+                    // push to picturebox
+                    Image image = Image.FromStream(memStream);
+                    Invoke((MethodInvoker)delegate () { pictureBoxImage.Image = image; });
+                }
+            }
+        }
+
+        // disconnect stops streming thread
+        private void ButtonDisconnect_Click(object sender, EventArgs e)
+        {
+            streamThread.Abort();
+            buttonConnect.Enabled = true;
+            buttonDisconnect.Enabled = false;
+            textBoxIp.Enabled = true;
+            textBoxPort.Enabled = true;
+            logText("Client disconnected");
+        }
+
+        // Delegate enables asynchronous calls for setting text property of textBox  
+        delegate void AddTextToLog(string text);
+
+        // generic logging function to print to screen
+        private void logText(string text)
+        {
+            if (textBoxOut.InvokeRequired)
+            {
+                AddTextToLog d = new AddTextToLog(logText);
+                Invoke(d, new object[] { text });
+            }
+            else
+            {
+                textBoxOut.AppendText("[" + DateTime.Now.ToString() + "] - " + text + Environment.NewLine);
+            }
+        }
+
+        // findIndex used to return index position of a byte pattern in a given byte[]
+        private static int findIndex(byte[] searchArray, byte[] patternArray)
+        {
+            if (patternArray.Length > searchArray.Length)
+                return -1;
+            for (int i = 0; i < searchArray.Length - patternArray.Length; i++)
+            {
+                bool found = true;
+                for (int j = 0; j < patternArray.Length; j++)
+                {
+                    if (searchArray[i + j] != patternArray[j])
+                    {
+                        found = false;
+                        break;
+                    }
+                }
+                if (found)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
